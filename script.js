acc4b6fcc0ff4a6e6ad40c96f5c4208192926a3c
^Felipe <felipesqf@gmail.com>
2020-09-24 23:33:32 +1000

    DOM changes appending recomandations to the cards

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 $(document).ready(function () {
   $("#searchB").on("click", function (event) {
+    // initiating upon clicking on the search
     event.preventDefault();
 
-    var userOption = $("#userOption").val();
+    var userOption = $("#UserSearch").val();
     var APIKey = "385709-project1-WLBPHP8D";
 
     var divQueryURL =
@@ -16,8 +17,17 @@ $(document).ready(function () {
     console.log(divQueryURL);
 
     $.getJSON(divQueryURL).then(function (response) {
+      //API tasteDive
       console.log(response);
+
       for (var i = 0; i < response.Similar.Results.length; i++) {
+        //looping at the recommendations and add on the cards
+        var typeID = "#Sug" + i;
+        var SuggText = "#Suggestion" + i;
+        $(typeID).text(response.Similar.Results[i].Type);
+        $(SuggText).text(response.Similar.Results[i].Name);
+
+        //passing each recommandations to the wiki api
         wikiAPI =
           "https://en.wikipedia.org/w/api.php?action=query&prop=extracts&exsentences=10&origin=*&exlimit=1&titles=" +
           response.Similar.Results[i].Name +
