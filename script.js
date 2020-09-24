0585b6036ac72b02b678c4c8d3f7dded6012c6b1
^Felipe <felipesqf@gmail.com>
2020-09-24 21:24:20 +1000

    APIs connected

--- script.js
+++ script.js
@@ -1,25 +1,36 @@
-$(document).ready(function(){
-
-$("#searchB").on("click", function(event){
+$(document).ready(function () {
+  $("#searchB").on("click", function (event) {
     event.preventDefault();
-    
-var userOption= $ ("#userOption").val();
-var APIKey ="385709-project1-WLBPHP8D"
 
-var divQueryURL ="https://tastedive.com/api/similar?info=1&origin=*&q="+ userOption +"&k="+APIKey;
+    var userOption = $("#userOption").val();
+    var APIKey = "385709-project1-WLBPHP8D";
 
-console.log(userOption);
-console.log(divQueryURL);
+    var divQueryURL =
+      "https://tastedive.com/api/similar?info=1&origin=*&q=" +
+      userOption +
+      "&k=" +
+      APIKey +
+      "&callback=?";
 
-$.ajax({
-   url: divQueryURL,
-   method:"GET"
-}).then(function(response){
-    // console.log(response);
+    console.log(userOption);
+    console.log(divQueryURL);
+
+    $.getJSON(divQueryURL).then(function (response) {
+      console.log(response);
+      wikiAPI(response.Similar.Results[0].Name);
+    });
+  });
 });
+function wikiAPI(name) {
+  wikiAPI =
+    "https://en.wikipedia.org/w/api.php?action=query&prop=extracts&exsentences=10&origin=*&exlimit=1&titles=" +
+    name +
+    "&explaintext=1&format=json";
 
-});
-       
-
-
-});
+  $.ajax({
+    url: wikiAPI,
+    method: "GET",
+  }).then(function (response) {
+    console.log(response);
+  });
+}
